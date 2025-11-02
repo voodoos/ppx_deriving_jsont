f902067a00469ddea4bf23411a0a796604c765fd
Ulysse Gérard <thevoodoos@gmail.com>
2025-11-02 16:08:28 +0100

    More tests

--- tests/tests.ml
+++ tests/tests.ml
@@ -1,12 +1,29 @@
-type enum = A | X [@key "B"] | C [@@deriving jsont]
-type var = V of enum [@key "V2"] | D [@@deriving jsont]
-type 'a t = { parent : 'a t option; [@option] v : 'a } [@@deriving jsont]
-
 let print_and_recode j t =
   let open Jsont_bytesrw in
   let json = encode_string j t |> Result.get_ok in
   print_endline json;
   decode_string j json |> Result.get_ok
 
-let v = { parent = Some { parent = None; v = V X }; v = D }
-let () = assert (v = print_and_recode (jsont var_jsont) v)
+type enum = A | X [@key "B"] | C [@@deriving jsont]
+
+let v = [ A; X; C ]
+let () = assert (v = print_and_recode (Jsont.list enum_jsont) v)
+
+type u = { name : v; next : u option } [@@deriving jsont]
+and v = enum
+
+let v : u = { name = A; next = Some { name = X; next = None } }
+let () = assert (v = print_and_recode u_jsont v)
+
+type 'a t = { name : string option; [@option] v : 'a var } [@@deriving jsont]
+and 'a var = V of enum [@key "V2"] | D of 'a t
+
+let v = { name = None; v = D { name = Some "d"; v = V A } }
+let () = assert (v = print_and_recode (jsont jsont) v)
+
+(*
+
+let rec jt_rec_t type_param_a =
+  lazy ... Jsont.rec' (jt_rec_t type_param_a) ...
+in
+*)
